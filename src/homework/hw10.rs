2681efead2670e8463b6aead43ee019848a4db53
Mavch <danil.movchan@e-u.edu.ua>
2025-05-13 13:17:58 +0300

    1234

--- src/homework/hw10.rs
+++ src/homework/hw10.rs
@@ -1,3 +1,23 @@
+pub fn rotate(s: String, n: isize) -> String {
+    let len = s.chars().count();
+    if len == 0 {
+        return s;
+    }
+
+    let shift = (n.rem_euclid(len as isize)) as usize;
+    if shift == 0 {
+        return s;
+    }
+
+    let chars: Vec<char> = s.chars().collect();
+    let split_at = len - shift;
+
+    let mut rotated = String::with_capacity(s.len());
+    rotated.extend(chars[split_at..].iter());
+    rotated.extend(chars[..split_at].iter());
+    rotated
+}
+
 pub fn is_palindrome(x: u32) -> bool {
     let original = x;
     let mut n = x;
@@ -20,7 +40,27 @@ pub fn is_palindrome(x: u32) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use super::is_palindrome;
+    use super::{rotate, is_palindrome};
+
+    #[test]
+    fn test_rotate() {
+        let s = "abcdefgh".to_string();
+        let shifts = [
+            (0,  "abcdefgh"),
+            (8,  "abcdefgh"),
+            (-8, "abcdefgh"),
+            (1,  "habcdefg"),
+            (2,  "ghabcdef"),
+            (10, "ghabcdef"),
+            (-1, "bcdefgha"),
+            (-2, "cdefghab"),
+            (-10,"cdefghab"),
+        ];
+
+        shifts.iter().for_each(|(n, exp)| {
+            assert_eq!(rotate(s.clone(), *n), exp.to_string());
+        });
+    }
 
     #[test]
     fn test_palindromes() {
